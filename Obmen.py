447123d4b829c8ed48d3319c41cb11214d8670c5
tagor2008g-coder <tagor2008g@g,com>
2025-08-28 22:46:46 +0300

    создана функция exchange для получения курсов валют из интернета

--- Obmen.py
+++ Obmen.py
@@ -3,6 +3,26 @@ from tkinter import messagebox as mb
 import requests
 import json
 
+def exchange():
+    code = entry.get().upper()
+    if code:
+        try:
+            response = requests.get(f'https://www.cbr-xml-daily.ru/daily_json.js')
+            response.raise_for_status()
+            data = response.json()
+
+            if code in data['rates']:
+                exchange_rate = data['rates'][code]
+
+                mb.showinfo("Курс обмена", f"Курс к доллару: {exchange_rate:.1f} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Ошибка: {e}")
+        else:
+            mb.showwarning("Внимание", "Введите код валюты")
+
+
 window=Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
